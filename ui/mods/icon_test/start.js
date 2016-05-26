d9a1a3b96f1dc3b5d090ca1e182615de14d9c6cb
Justin Love <git@JustinLove.name>
2016-05-26 17:51:31 -0500

    missed start value set

--- ui/mods/icon_test/start.js
+++ ui/mods/icon_test/start.js
@@ -5,9 +5,18 @@ api.memory.list('icon_test_number').then(function(list) {
       type: 'text',
       default: ''
     }
-    var value = api.settings.isSet('ui', 'icon_test_number', true)
-    api.memory.store('icon_test_number', value).then(function() {
-      api.file.mountMemoryFiles({})
+    api.settings.definitions.ui.settings['icon_test_delay'] = {
+      title: 'Delay Seconds',
+      type: 'text',
+      default: '5'
+    }
+    var number = api.settings.isSet('ui', 'icon_test_number', true)
+    var delay = api.settings.isSet('ui', 'icon_test_delay', true)
+    api.memory.store('icon_test_number', number).then(function() {
+      api.memory.store('icon_test_delay', delay).then(function() {
+        api.file.mountMemoryFiles({})
+      })
     })
   }
 })
+
