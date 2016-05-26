65e0d497ac4d9515094bc54e232aa4119a823af8
Justin Love <git@JustinLove.name>
2016-05-26 17:10:04 -0500

    configurable delay time

--- ui/mods/icon_test/icon_atlas.js
+++ ui/mods/icon_test/icon_atlas.js
@@ -1,11 +1,15 @@
-api.memory.load('icon_test_number').then(function(value) {
-  var number = parseInt(value, 10)
-  console.log(value, number)
+api.memory.load('icon_test_number').then(function(numberText) {
+  api.memory.load('icon_test_delay').then(function(delayText) {
+    var number = parseInt(numberText, 10)
+    console.log(numberText, number)
+    var delay = parseInt(delayText, 10) * 1000
+    console.log(delayText, delay)
 
-  var icons = model.strategicIcons()
-  for (var i = icons.length;i < number;i++) {
-    icons.push('icon_' + i.toString())
-  }
-  model.strategicIcons.valueHasMutated()
-  setTimeout(model.sendIconList, 5000)
+    var icons = model.strategicIcons()
+    for (var i = icons.length;i < number;i++) {
+      icons.push('icon_' + i.toString())
+    }
+    model.strategicIcons.valueHasMutated()
+    setTimeout(model.sendIconList, delay)
+  })
 })
--- ui/mods/icon_test/settings.js
+++ ui/mods/icon_test/settings.js
@@ -4,6 +4,11 @@
     type: 'text',
     default: ''
   }
+  api.settings.definitions.ui.settings['icon_test_delay'] = {
+    title: 'Delay Seconds',
+    type: 'text',
+    default: '5'
+  }
 
   // force model.settingsLists to update
   model.settingDefinitions(api.settings.definitions)
@@ -15,12 +20,24 @@
     })
   })
 
+  model.iconTestDelay = model.settingsItemMap()['ui.icon_test_delay'].value
+  model.iconTestDelay.subscribe(function(value) {
+    api.memory.store('icon_test_delay', value).then(function() {
+      api.file.mountMemoryFiles({})
+    })
+  })
+
+
   var settingsHtml = 
     '<div class="form-group">' +
       '<div class="sub-group">' +
         '<div class="option">' +
           '<label>Min Icons</label>' +
           '<input type="text" class="form-control" value="" data-bind="value: iconTestNumber" />' +
+        '</div>' + 
+        '<div class="option">' +
+          '<label>Delay Seconds</label>' +
+          '<input type="text" class="form-control" value="" data-bind="value: iconTestDelay" />' +
         '</div>' + 
       '</div>' + 
     '</div>'
